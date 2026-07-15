0edbc16e2d8603da4a90d4c51b7fc7d2324dda2d
RetroFrost <routerifter@gmail.com>
2026-07-15 17:12:46 -0300

    Add CTS Android activity entry point

---
+++ android/app/src/main/java/io/github/retrofrost/cts/android/MainActivity.kt
@@ -0,0 +1,20 @@
+package io.github.retrofrost.cts.android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import io.github.retrofrost.cts.android.ui.CtsAndroidApp
+import io.github.retrofrost.cts.android.ui.theme.CtsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CtsTheme {
+                CtsAndroidApp()
+            }
+        }
+    }
+}
