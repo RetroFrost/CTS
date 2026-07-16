f26c2ac47057b805651505835a0c48372b2e09a8
RetroFrost <routerifter@gmail.com>
2026-07-16 12:28:32 -0300

    Bump Android Alpha 9 reference video build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "io.github.retrofrost.cts.android"
         minSdk = 23
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.1.0-alpha8-length"
+        versionCode = 9
+        versionName = "0.1.0-alpha9-video-clone"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
