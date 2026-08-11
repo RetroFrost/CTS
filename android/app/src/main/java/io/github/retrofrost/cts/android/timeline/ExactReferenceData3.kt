64dcfe62da5faa1a2f39655de37a92c7c4ec4503
RetroFrost <routerifter@gmail.com>
2026-08-11 15:38:02 -0300

    Add exact badge, outro and fade state data

---
+++ android/app/src/main/java/io/github/retrofrost/cts/android/timeline/ExactReferenceData3.kt
@@ -0,0 +1,77 @@
+package io.github.retrofrost.cts.android.timeline
+
+internal const val MALES_OPENING_BADGE =
+        "eNrtltdXU/kWxwEFFRVkhgsqRUBEpOhI0yTnnC0JosCIgCIWbIMCUtTRKyAWQqipJFICUo0BCwICKiYnB/I7YJ+LjoidYhkUFREU" +
+        "bOPl+nT/gbvum3utz1rftdf3Ye/13Q9bR0ebovODH/zgB/9H4hAL19pHkqMupcSyyQYgWXmOI1JEI1v6Km7I+KIZftBBeDx2BO/Y" +
+        "Oo4jtpZ+FGRLzLq0gIoiFcRDC4BE/ZOci4xldNy2zQTXyIz6/Go7cc8iGCovlHHcMEfa5ZWIsL6hQ/mz3Yj5ynB4bFLA4cyfReO/" +
+        "VhLN1fc1ZbQpcXb9NljHlnBqdljRzwu1RBc7T8Peq0OkhMbA6GAGp+Ta937APaLlW6amVKFL3PbYBa42hzmFNS50VOEAUeDtpXkd" +
+        "MI5gG+6Biv5dnA8cD7rtqC5kzrbUdNydROTH7YNynQjOxNYltPmEKTBfYK8pTjQlsuVJcNQmjNNvFUJf8TIDy+NTNQz1dCJRkQyd" +
+        "zACOU10YbTHJBnyWb9RIIx2Jw9cPQdgibw6qjaeLJs2DJJsyjfSFG2GfmAKbDBdzLBwP0G98F8IqmqOpHnAjtmm5YJ/iwqlOOkCP" +
+        "i2DChDUsTdQlFjHxX6ngsNeOk+aZRjcc8QYfRx/NoaM+xIMkHqzNmcGZkJVFT+zyg4KIIo2BIoRQhKZB8IAJR7xDQkcZBsPy4tUa" +
+        "UcRGIqciDUyMJ3K2ZUnpQHkYzHFL09w+kUyMs02D9/k6nFmLC2m7pxuhoiVbgxqyiZhKHrQaD7Ob+0roWcbbQW96nMZ2ewLxzTwD" +
+        "OtOfsD2eFdGTomLBwTxRE5QdRbR+yYCfDnawZUI5vcj/d0jomq3x/RJIXLbOgj2xreytdVI6ST8R+rI/ks3h3kSoRxbUqc6xxycL" +
+        "6TsvD0CcZ5fa/P5C4s/QLFjcWMmufJVO67txwWHrWZblS2Mi+jcBrFgpZ6cQPHpGOw8sJCJsom47fny3GMo6M9m0237aPCgDkvFR" +
+        "rFB7CGc+yIG21Hi2zbsY+i+nbDBjhOPzHdKxL0ky0Pf3Yc9rC6J3nONDkXUYvmBwFgbHpUAp3NhuvWzaq4MP/H5TXKk7B3v8Qgaf" +
+        "JrizU+YF0pk/C8FPOh43fJ2tnnHjCJQkzGXv6Q6gC17z4eb6Gdjfz9ZRN8rk8CoQ2JrBdbTNcDbsncfFcqsE1NsPhaBrv4y9wSiE" +
+        "3qSfCbr1A5hD+w0q4aACmKpQ9vKebFqzIAOaV/CwaKMWys/+GDzdt4N9ScijBR3pUDX+M6vb7hS1g1EBXX/EsTtruPRvFlI4pFqD" +
+        "m864SVX9XQz+Zvne6oYwumSlFKb/wcCH6tTUhpdF8FJX7M24u5zu7ZRCyMpP2HR9CXWGLgbOCal3LRVLj/GPQJS+ET5smkSNbSqB" +
+        "OYd+8ZbZH6R/uXIE3HJ88cuSY9RI3VGQZ51fYuG8kd4lyIWYnUvwyUOnqX7DIjh7O25J9+5ltHBpHmScccfrExoov3/Koadh0pJw" +
+        "xUK67VYezGubjUduaKQS1haAhUkFWK6cTRvG5cOtYGtclnqGSsnOh8GSn1rqnCxpjMiHmKvz8VbPs1TMhzxIifHVYioT+nNrPog2" +
+        "WeL+XlWUza48UJnKtfuajeiS+AI462yEv/tYTvk9ygWepEdrenMSPewph0vOJnh0bBl1g5UL31S2aK5Ijy4/LwdB7WS8cONRanv3" +
+        "EVB8YKCke99Qp1MhLOsxxcPZpdT9qCMQVrYOlWa9QydlhfCz0ho3Gyim6npksPrvBMSY3YfKawrhpCWGf01RUsaZMui2zkH1RnfQ" +
+        "7MxCsL0QiDeVVFJeTjLYmVWK0l2uIduphdCvtwaPPqqk3o1JwWO3AjF+b0XmF+RgZRCCd4qPU14qKVz/eBLFr9GgwUA5xDHW467P" +
+        "lNSiPVKotWtAyVMb0TUjOfDjPHFycz612lkK5r1nUOI3NVKeKoBQ91n4nu/3FvM6By5aV6OqBxQqVxZA92IDvGlyKlVblQPaJ7XI" +
+        "yq0FldsWwP7nX7HcgGTKdn8OPJlTi06/1aDjvfnwsv4FFua+k3rnkQN2RD1atlONeMH5IHx4F+v030rJLHJgoU0NujaJRFydfDjZ" +
+        "1Y31tW2gtryUQP/mRtRjfwFFHcuD2psaTDviTy04L4GEP+tQn30T+uolAQNfDh5JCVhed/kwGjaE+mSV6LGeGCI3T8GXlv+MrQgX" +
+        "AOPFbUTrnUOhX4UQMOEydk/Dxk72C2DmXxcRn9eC0vcJwYUTibleWI2dZgvBdlolWp92Ca2qEELCNgbmtGkZFr9FCPHLlaiksw0d" +
+        "LBCC8QpTrNPDC3PfL4QlJyqQsVsb4mcJYe63PlZa+wJsQCyEVUMlqMGhDfWxRSDy6WPp9Rlg78KFsDbzJCIGNUi6RARNd+pYDxcN" +
+        "s2TRQohwrkJ3GkjUholAXZTOeju+nWW2VwgF5kpkNEGNJjiLoXrRJ2bJZQkrOFQIB+W1aKjnHOqaI4YU/VjmTA8Xlvt6IeDBNWjo" +
+        "aD1CpmJQ5m5Xr9nwCxPFCaGx5hRSBtWiK7piwKRTybvJ5uS4CCG4RlShktgT6O0bEUjiikkhdYpcGiKEV9IKFDG5AhmQYtAn7pPj" +
+        "PqrIIT0hdCSeRnmWxWivrgTWVptrroeHkuu6BPDMvRopP5YiRZUY/MaNkEUh19U+9wSg3lON/K2OobtJYnDJ3km+99QnvUcEIMDr" +
+        "kdpGgeZ4imGo/Jw6dwGTDB4TQKusAXmMViCv2WL4pnQnrY1CyRW6QrhcU4/i11YgTagYvGtDyJth0aTf9xwZs86jyavKkKtKBBZv" +
+        "Asmi27+R+777o7/vOnF6OZpWK4Jf3xeQGrMR9a03AnAtq0avllSgu+ki6M/bTK4uiyA9vs9zmleNai1L0culIuAW+pOWr6Sk/kQh" +
+        "5JqeQk5Hi5GxGqQi2HleSnbIwctpHAZReO45kshI0f0QI7E9KcuEfzuS6GwJojFOgX82LUeUBIQzM30XKHezI7goBYC7lyPBUIVJd" +
+        "E0DL5IXk1TZavXWvALp981DQajnyiRIAH91S3/wSpvZwF8CeiBxU15CH1r/gg3pn+uIzTd5qtzE+pA3zkcMFGVrO44Prbopx3lFf" +
+        "Dd18SPLPQOs8pWj3Fj40HmxSfX2fzDis5MMIi4e2ciTonTcfrvcGqtT9DYxZ5nyYHc9FVopMlO/KB5MoO7U+35i5oTYbuO0H0dN6" +
+        "LupQZcGc3uFFpoO6zMOfs+Bb9io0YhSHtsqyoG+l00WuyVTGjuVZYK8TiHour0H8/Zmw+9/XVKo1cUxrRSb4LZiHRp/6oCtTMmCZ" +
+        "u1o1vekfzJLFGfDkymdt6CYLtGpLOkg6A9Rpk+YzvSXpsL+/UysLnIgWHEmDqWeMSeU6BbMqNQ3sX+dp3/be10YapIHF1yJV9Fgh" +
+        "I9opDZpW8LQFiyitbzEP6kMj1W2PdjJjr/NA0hGsTf18WpsUzwMPZ7b66uEgZpARD562gvYLOqx1fJIKZXc61DyzJmZ7eyrUiG+0" +
+        "/DUWpX2lkwo+bbTK+OwU5sO9qTDz+bnmeebuWskoF876x6hcFq1hHGOmQm9iSbNRsKl2Yz8XHmQ8UI3recmwNEgFDx/H5u4QXW1x" +
+        "x/d/o/G56lHvT8ziUi50dJ6jrsafaJl2lQsxj/nqsO4Y5tjvXNgzmKFJEPBbRmdy4d9N8arnmwcY7wO4kKzzmdhWO7flugEXbDf6" +
+        "qoOttAy7uVxgWulBVLq6WUeHCzr/rR/6f9X/AWmy3iU="
+
+internal const val MALES_POST_BADGE =
+        "eNrtl89OE1EUxs9JjFFjSENB8U8aQhokhBBDCCGEEEJKSVzYmVIQ8U6jiDigAiHAwhVP4MqlD+DSR3LjO+CfqN/c3tZhuHfmztiy" +
+        "0GYWv5zv9jv3zJlzpxk6oe7VvbpX9/qvrzd0nS5zlargND+VnOctcsEF3lV8Sw5Y4hMVv5Ms8XuqSH5Q6x/V+ie1/lnmK/EXxNfA" +
+        "QW7SARe5INmMA1bAcut3hXN6k86ZPEWlj0pfmSekXuYpxVnJJS5xUOcSlzmou84PFasc1Cu4JtfrvK70utI3pe7xllr31fpruZ/g" +
+        "faUfKP2wRYd64GuwoeeUnlN6gxXqg74P9oN70G9A3w3pDZ73mfLljPuFdRcsKb2k9HKIgb6o8UXzOYY6dPuJhHzt9Nn0q57Qr3b1" +
+        "WST4hEWfTftVYtZN+3gJ63WL+wvHoo396lSf085R9P6y1CFSzLNNX4RFXyqWPtcyn22fdfNgM39JPtM82/X5QNKT7JXv62W8T9fx" +
+        "/l6hAVrDe31V8RHdplXe4DWwJnmXVvA/8BisgetUoGUWkrUQn4BVGQ+Ca4j1FKAbo4uI7oFOgs/R+ETEJzT5HE0+z5AvTZ02Ps9Q" +
+        "XzOO5vNCv3MtfW4Co76kfI5mXRjqbPo8TX43pl9p+xJ333F1xuWr/kWf09Sv83kZ+2x6zqb78xLOR9o+63xxffEy9CPr+XFTzLNn" +
+        "eO6m/XTnvJrCl/YcRM9V2vnrxDy7Cc8vqY4/eVbBIfjwT0NFGsV3yQYNUxHfKc9ohO7xA34ODoObNIa4JDkCvgDv8ALicTD4ghun" +
+        "W+BL8KbiAOjTfbrBc4jPMtD78Z3UWJ/lbbBPxWFu0wQ406IfivMa3Y/8XreeT8hnWjcxTT6TL6rvWOQ15cu3sV/RvGl8vRn7bHN/" +
+        "0Tp2LPYz5WunL1qH7XPoZJ+z3l/W+nX5/DbPs01f+jp0DtL2WTcP4TnJW8yRn+ALvzcu4hwk9SVap5/B16viHE+Dk+AUvwJ7eJL3" +
+        "aJqu8jjv0gxd4THEs3SJR0KcA4t8AJLiLwo4Tz9piA/BHxF+p0FwAXGhxSPwW4iHiscGPYi/xvji8p1q9Iv2hfVTi/qPLH3HlnXY" +
+        "7vev8jdxT5zE"
+
+internal const val MALES_COVER =
+        "eNpjYMAEMgweDA0MRxgEGVMZ9zKKMxUxnWKSY85iXsf8nFmIRY/FgggIAKymCuE="
+
+internal const val MALES_CONTENT_Y =
+        "eNpjaGDAAfX+6/33/F/wf8r/Tf/P/n/0/9P/P/8ZyAIAJuoWbw=="
+
+internal const val MALES_FADE =
+        "eNr7+/fXzw9vnz5+9PDe3du3bly/fOHs6WN7Nm1bs3LZklnTJk/s6WqsLS7IyUpNCg8J8HVxsLbQ11BVVpAUFeLnYmNkQAYAIpEh" +
+        "fw=="
+
+internal const val RELATIONSHIPS_FADE =
+        "eNoBNgDJ//v59O/s6OPg3NbTz8vHxMC0sKyoo5+bmJOOi4eDf3p2cm9jX1tXU05LR0I/OjUyLSolIR4XEvoYHLk="
+
+internal const val MALES_STARTS =
+        "eNoBnABj/wAAeADwAGgBEAK9ApMDaQQ/BRYG7AbCB5gIbwlFChsL8gvJDJ4NdA5LDyEQ9xDOEaQSehNQFCcV/RXTFqkXfxhVGSwa" +
+        "AhvYG68chR1aHjEfByDdILQhiiJgIzYkDSXjJbkmiidhKDcpDSrkKrorkCxmLTwuEi/pL78wlTFrMkIzGDTuNMQ1mjZwN0c4HTnz" +
+        "Oco6oDt2PEw9Iz75Po5uL7U="
+
+internal const val RELATIONSHIPS_STARTS =
+        "eNoBUACv/3YBCQKQAhsDhAOPBJgFoQaqB7MIvAnFCs4L1wzgDekO8w/8EAUSDhMXFCAVKRYyFzsYRBlOGlcbYBxpHXIeex+EII0h" +
+        "liKfI6kksiW7JsQnjI8XUQ=="
