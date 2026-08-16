7c1deece3a3e3021d32c5ef6c90be0535bddf3bd
RetroFrost <routerifter@gmail.com>
2026-08-16 10:35:09 -0300

    Route engine through release WatchData 2.0.4 layer

--- engine/ccengine/__init__.py
+++ engine/ccengine/__init__.py
@@ -4,13 +4,15 @@ from . import renderer as _renderer
 from .watchdata_renderer import WatchDataFrameRenderer
 from .watchdata_measured import MeasuredWatchDataFrameRenderer
 from .watchdata_final import FinalWatchDataFrameRenderer
+from .watchdata_release import ReleaseWatchDataFrameRenderer
 
-_renderer.FrameRenderer = FinalWatchDataFrameRenderer
-FrameRenderer = FinalWatchDataFrameRenderer
+_renderer.FrameRenderer = ReleaseWatchDataFrameRenderer
+FrameRenderer = ReleaseWatchDataFrameRenderer
 
 __all__ = [
     "FrameRenderer",
     "WatchDataFrameRenderer",
     "MeasuredWatchDataFrameRenderer",
     "FinalWatchDataFrameRenderer",
+    "ReleaseWatchDataFrameRenderer",
 ]
