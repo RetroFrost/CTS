3c7c643e4303ba25bea88d56a261fb07a692b6a7
RetroFrost <routerifter@gmail.com>
2026-09-05 20:06:03 +0100

    Add .NET Windows application entry point

---
+++ windows-dotnet/Program.cs
@@ -0,0 +1,27 @@
+using System.Windows;
+
+namespace CubicalCompare.Windows;
+
+public static class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        var app = new Application
+        {
+            ShutdownMode = ShutdownMode.OnMainWindowClose,
+        };
+        app.DispatcherUnhandledException += (_, e) =>
+        {
+            try
+            {
+                var report = $"Cubical Compare 3.0.300 Windows\r\n{DateTimeOffset.Now:O}\r\n\r\n{e.Exception}";
+                Clipboard.SetText(report);
+                MessageBox.Show("Cubical Compare hit an unexpected error. The crash report was copied to the clipboard.\n\n" + e.Exception.Message, "Cubical Compare", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch { }
+            e.Handled = true;
+        };
+        app.Run(new MainWindow());
+    }
+}
