a55a06e9b749dc832c2572a178f9b2b043a9764e
RetroFrost <routerifter@gmail.com>
2026-09-14 10:06:47 +0100

    Make Zipack2 models WinUI bindable and add thumbnail metadata

--- windows/CubicalCompare/Core/MegaPack/Zipack2Models.cs
+++ windows/CubicalCompare/Core/MegaPack/Zipack2Models.cs
@@ -5,88 +5,91 @@ namespace CubicalCompare.Core.MegaPack;
 public sealed class Zipack2Manifest
 {
     [JsonPropertyName("format")]
-    public string Format { get; init; } = "cubical.megapack.zipack2";
+    public string Format { get; set; } = "cubical.megapack.zipack2";
 
     [JsonPropertyName("version")]
-    public int Version { get; init; } = 2;
+    public int Version { get; set; } = 2;
 
     [JsonPropertyName("name")]
-    public string Name { get; init; } = "MegaPack";
+    public string Name { get; set; } = "MegaPack";
+
+    [JsonPropertyName("thumbnail")]
+    public string Thumbnail { get; set; } = "";
 
     [JsonPropertyName("contactSheets")]
-    public List<Zipack2ContactSheetDefinition> ContactSheets { get; init; } = [];
+    public List<Zipack2ContactSheetDefinition> ContactSheets { get; set; } = [];
 
     [JsonPropertyName("cards")]
-    public List<Zipack2CardDefinition> Cards { get; init; } = [];
+    public List<Zipack2CardDefinition> Cards { get; set; } = [];
 
     [JsonPropertyName("show_badges")]
-    public bool ShowBadges { get; init; } = true;
+    public bool ShowBadges { get; set; } = true;
 
     [JsonPropertyName("credits_enabled")]
-    public bool CreditsEnabled { get; init; } = true;
+    public bool CreditsEnabled { get; set; } = true;
 
     [JsonPropertyName("duration_seconds")]
-    public double DurationSeconds { get; init; }
+    public double DurationSeconds { get; set; }
 }
 
 public sealed class Zipack2CardDefinition
 {
     [JsonPropertyName("id")]
-    public string Id { get; init; } = "";
+    public string Id { get; set; } = "";
 
     [JsonPropertyName("title")]
-    public string Title { get; init; } = "";
+    public string Title { get; set; } = "";
 
     [JsonPropertyName("name")]
-    public string Name { get; init; } = "";
+    public string Name { get; set; } = "";
 
     [JsonPropertyName("value")]
-    public string Value { get; init; } = "";
+    public string Value { get; set; } = "";
 
     [JsonPropertyName("badge_header")]
-    public string BadgeHeader { get; init; } = "";
+    public string BadgeHeader { get; set; } = "";
 
     [JsonPropertyName("badgeHeader")]
-    public string BadgeHeaderCamel { get; init; } = "";
+    public string BadgeHeaderCamel { get; set; } = "";
 
     [JsonPropertyName("badge_primary")]
-    public string BadgePrimary { get; init; } = "";
+    public string BadgePrimary { get; set; } = "";
 
     [JsonPropertyName("badge_secondary")]
-    public string BadgeSecondary { get; init; } = "";
+    public string BadgeSecondary { get; set; } = "";
 
     [JsonPropertyName("description")]
-    public string Description { get; init; } = "";
+    public string Description { get; set; } = "";
 
     [JsonPropertyName("details")]
-    public string Details { get; init; } = "";
+    public string Details { get; set; } = "";
 
     [JsonPropertyName("image_x")]
-    public double ImageX { get; init; }
+    public double ImageX { get; set; }
 
     [JsonPropertyName("image_y")]
-    public double ImageY { get; init; }
+    public double ImageY { get; set; }
 
     [JsonPropertyName("image_scale")]
-    public double ImageScale { get; init; } = 1.0;
+    public double ImageScale { get; set; } = 1.0;
 
     [JsonPropertyName("image_rotation")]
-    public double ImageRotation { get; init; }
+    public double ImageRotation { get; set; }
 
     [JsonPropertyName("image_crop_left")]
-    public double ImageCropLeft { get; init; }
+    public double ImageCropLeft { get; set; }
 
     [JsonPropertyName("image_crop_top")]
-    public double ImageCropTop { get; init; }
+    public double ImageCropTop { get; set; }
 
     [JsonPropertyName("image_crop_right")]
-    public double ImageCropRight { get; init; }
+    public double ImageCropRight { get; set; }
 
     [JsonPropertyName("image_crop_bottom")]
-    public double ImageCropBottom { get; init; }
+    public double ImageCropBottom { get; set; }
 
     [JsonPropertyName("image_layer")]
-    public string ImageLayer { get; init; } = "behind";
+    public string ImageLayer { get; set; } = "behind";
 
     public Zipack2CardData Normalize(int index)
     {
@@ -117,83 +120,83 @@ public sealed class Zipack2CardDefinition
 
 public sealed class Zipack2CardData
 {
-    public string Id { get; init; } = "";
-    public string Title { get; init; } = "";
-    public string Value { get; init; } = "";
-    public string BadgeHeader { get; init; } = "";
-    public string Description { get; init; } = "";
-    public double ImageX { get; init; }
-    public double ImageY { get; init; }
-    public double ImageScale { get; init; } = 1.0;
-    public double ImageRotation { get; init; }
-    public double ImageCropLeft { get; init; }
-    public double ImageCropTop { get; init; }
-    public double ImageCropRight { get; init; }
-    public double ImageCropBottom { get; init; }
-    public string ImageLayer { get; init; } = "behind";
+    public string Id { get; set; } = "";
+    public string Title { get; set; } = "";
+    public string Value { get; set; } = "";
+    public string BadgeHeader { get; set; } = "";
+    public string Description { get; set; } = "";
+    public double ImageX { get; set; }
+    public double ImageY { get; set; }
+    public double ImageScale { get; set; } = 1.0;
+    public double ImageRotation { get; set; }
+    public double ImageCropLeft { get; set; }
+    public double ImageCropTop { get; set; }
+    public double ImageCropRight { get; set; }
+    public double ImageCropBottom { get; set; }
+    public string ImageLayer { get; set; } = "behind";
 }
 
 public sealed class Zipack2ContactSheetDefinition
 {
     [JsonPropertyName("path")]
-    public string Path { get; init; } = "";
+    public string Path { get; set; } = "";
 
     [JsonPropertyName("order")]
-    public int Order { get; init; }
+    public int Order { get; set; }
 
     [JsonPropertyName("expectedCardWidth")]
-    public int? ExpectedCardWidth { get; init; }
+    public int? ExpectedCardWidth { get; set; }
 
     [JsonPropertyName("expectedCardHeight")]
-    public int? ExpectedCardHeight { get; init; }
+    public int? ExpectedCardHeight { get; set; }
 
     [JsonPropertyName("separator")]
-    public Zipack2SeparatorDefinition Separator { get; init; } = new();
+    public Zipack2SeparatorDefinition Separator { get; set; } = new();
 
     [JsonPropertyName("regions")]
-    public List<Zipack2RegionDefinition> Regions { get; init; } = [];
+    public List<Zipack2RegionDefinition> Regions { get; set; } = [];
 }
 
 public sealed class Zipack2SeparatorDefinition
 {
     [JsonPropertyName("red")]
-    public byte Red { get; init; } = 255;
+    public byte Red { get; set; } = 255;
 
     [JsonPropertyName("green")]
-    public byte Green { get; init; } = 255;
+    public byte Green { get; set; } = 255;
 
     [JsonPropertyName("blue")]
-    public byte Blue { get; init; } = 0;
+    public byte Blue { get; set; } = 0;
 
     [JsonPropertyName("tolerance")]
-    public int Tolerance { get; init; } = 20;
+    public int Tolerance { get; set; } = 20;
 
     [JsonPropertyName("minimumCoverage")]
-    public double MinimumCoverage { get; init; } = 0.72;
+    public double MinimumCoverage { get; set; } = 0.72;
 
     [JsonPropertyName("minimumCardWidth")]
-    public int MinimumCardWidth { get; init; } = 48;
+    public int MinimumCardWidth { get; set; } = 48;
 
     [JsonPropertyName("minimumCardHeight")]
-    public int MinimumCardHeight { get; init; } = 48;
+    public int MinimumCardHeight { get; set; } = 48;
 }
 
 public sealed class Zipack2RegionDefinition
 {
     [JsonPropertyName("x")]
-    public int X { get; init; }
+    public int X { get; set; }
 
     [JsonPropertyName("y")]
-    public int Y { get; init; }
+    public int Y { get; set; }
 
     [JsonPropertyName("width")]
-    public int Width { get; init; }
+    public int Width { get; set; }
 
     [JsonPropertyName("height")]
-    public int Height { get; init; }
+    public int Height { get; set; }
 
     [JsonPropertyName("order")]
-    public int Order { get; init; }
+    public int Order { get; set; }
 }
 
 public readonly record struct PixelRect(int X, int Y, int Width, int Height)
@@ -204,33 +207,33 @@ public readonly record struct PixelRect(int X, int Y, int Width, int Height)
 
 public sealed class DetectedZipack2Card
 {
-    public required string SheetPath { get; init; }
-    public required int SheetOrder { get; init; }
-    public required int LocalIndex { get; init; }
-    public required int GlobalIndex { get; set; }
-    public required PixelRect Bounds { get; init; }
-    public required string ExtractedPath { get; init; }
-    public required double Confidence { get; init; }
+    public string SheetPath { get; set; } = "";
+    public int SheetOrder { get; set; }
+    public int LocalIndex { get; set; }
+    public int GlobalIndex { get; set; }
+    public PixelRect Bounds { get; set; }
+    public string ExtractedPath { get; set; } = "";
+    public double Confidence { get; set; }
     public Zipack2CardData? Data { get; set; }
 }
 
 public sealed class Zipack2SheetResult
 {
-    public required string Path { get; init; }
-    public required int Order { get; init; }
-    public required int Width { get; init; }
-    public required int Height { get; init; }
-    public required IReadOnlyList<DetectedZipack2Card> Cards { get; init; }
+    public string Path { get; set; } = "";
+    public int Order { get; set; }
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public IReadOnlyList<DetectedZipack2Card> Cards { get; set; } = [];
 }
 
 public sealed class Zipack2ImportResult
 {
-    public required string Name { get; init; }
-    public required string SourcePath { get; init; }
-    public required string ExtractionDirectory { get; init; }
-    public required IReadOnlyList<Zipack2SheetResult> Sheets { get; init; }
-    public required IReadOnlyList<DetectedZipack2Card> Cards { get; init; }
-    public required bool ShowBadges { get; init; }
-    public required bool CreditsEnabled { get; init; }
-    public required double DurationSeconds { get; init; }
+    public string Name { get; set; } = "MegaPack";
+    public string SourcePath { get; set; } = "";
+    public string ExtractionDirectory { get; set; } = "";
+    public IReadOnlyList<Zipack2SheetResult> Sheets { get; set; } = [];
+    public IReadOnlyList<DetectedZipack2Card> Cards { get; set; } = [];
+    public bool ShowBadges { get; set; } = true;
+    public bool CreditsEnabled { get; set; } = true;
+    public double DurationSeconds { get; set; }
 }
