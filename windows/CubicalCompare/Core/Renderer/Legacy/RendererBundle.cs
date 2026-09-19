eec28c9609f8241c1b98f0358f5f7aed22c6d1a3
RetroFrost <routerifter@gmail.com>
2026-09-19 18:47:29 +0100

    Set renderer app compatibility version to 4.2.1.4

--- windows/CubicalCompare/Core/Renderer/Legacy/RendererBundle.cs
+++ windows/CubicalCompare/Core/Renderer/Legacy/RendererBundle.cs
@@ -388,7 +388,7 @@ public static class RendererBundleReader
 
 public static class RendererCapabilities
 {
-    public const string AppVersion = "4.2.1.3";
+    public const string AppVersion = "4.2.1.4";
     public const int RendererApi = 3;
 
     private static readonly HashSet<string> Engines = new(StringComparer.Ordinal)
