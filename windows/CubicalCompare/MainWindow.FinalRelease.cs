4679c1339c6dc1e88708eae222cfc0282b2cfc8e
RetroFrost <routerifter@gmail.com>
2026-09-19 19:19:18 +0100

    Set updater fallback version to 4.2.1.5

--- windows/CubicalCompare/MainWindow.FinalRelease.cs
+++ windows/CubicalCompare/MainWindow.FinalRelease.cs
@@ -364,7 +364,7 @@ public sealed partial class MainWindow
     }
 
     private static Version GetCurrentAppVersion()
-        => Assembly.GetExecutingAssembly().GetName().Version ?? new Version(4, 2, 1, 4);
+        => Assembly.GetExecutingAssembly().GetName().Version ?? new Version(4, 2, 1, 5);
 
     private static Version NormalizeVersion(Version version) => new(
         Math.Max(0, version.Major),
